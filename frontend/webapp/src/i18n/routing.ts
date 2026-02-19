988347f3851864b007402d9e31811a9077929a52
Mohammad Jafariyan <aidin.khoshzaban@gmail.com>
2026-02-19 11:54:15 +0330

    language detection fix

--- frontend/webapp/src/i18n/routing.ts
+++ frontend/webapp/src/i18n/routing.ts
@@ -1,10 +1,8 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
   locales: ["en", "fa", "tr", "es", "ar", "ku", "de", "fr"],
-
-  // Used when no locale matches
   defaultLocale: "fa",
   localeDetection: true,
+  localeCookie: false // ✅ stop NEXT_LOCALE from overriding Accept-Language
 });
