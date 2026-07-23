af602e0e8ab880c9eb6ad67e01b18b89946f42c1
Mohammad Jafariyan <mohammad.jafariyan7@gmail.com>
2026-07-23 11:48:51 +0330

    hotfix

--- frontend/webapp/src/app/[locale]/n/app/mobile/home/page.tsx
+++ frontend/webapp/src/app/[locale]/n/app/mobile/home/page.tsx
@@ -448,7 +448,7 @@ async function Home({ params, searchParams }: PageProps) {
       <a referrerpolicy='origin' target='_blank' 
       href='https://trustseal.enamad.ir/?id=760932&Code=Q4tmSTcTQFTGWWFLYCxWTvO5hIsgD7Hr'>
         <img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=760932&Code=Q4tmSTcTQFTGWWFLYCxWTvO5hIsgD7Hr' 
-        alt='' style='cursor:pointer' code='Q4tmSTcTQFTGWWFLYCxWTvO5hIsgD7Hr' />
+        alt='' style={{"cursor":"pointer"}} code='Q4tmSTcTQFTGWWFLYCxWTvO5hIsgD7Hr' />
       </a>
    </div>
     </div>
